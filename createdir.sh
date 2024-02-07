44cbfebfeb91db7e7d3b3c13545b9e4fe287113a
mahesh2121 <maheshgaikwad128@gmail.com>
2024-02-07 12:07:45 +0530

    Scipt Directory and file creation

---
+++ createdir.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mkdir countries && cd countries
+
+mkdir USA india UK
+
+echo "Washington D.C" > USA/capital.txt
+echo "London" > UK/capital.txt
+echo "New Delhi" > india/capital.txt
+
+uptime
+
+
