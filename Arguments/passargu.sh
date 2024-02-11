17b953e8b37ae6d33b1532dfdcea3baf386c9d8c
mahesh2121 <maheshgaikwad128@gmail.com>
2024-02-11 22:01:01 +0530

    arguments

--- Arguments/passargu.sh
+++ Arguments/passargu.sh
@@ -1,4 +1,12 @@
 #! /bin/bash
 echo $0 $1 $2 $3 $4 '>$1 $2 $3 $4'
 args=("$@")
-echo ${args[1]} ${args[2]} ${args[3]} ${args[4]} 
+echo ${args[0]} ${args[1]} ${args[2]} ${args[3]} ${args[4]} 
+
+# calling all arguments
+
+echo $@
+
+# calling Number arguments
+
+echo $#
